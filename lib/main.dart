7a899f13829a3d2ba3a4ad7d226eb65f6408a6da
Assaduzzaman Ashik <ashikhossain372@gmail.com>
2025-10-16 14:06:10 +0600

    card add

--- lib/main.dart
+++ lib/main.dart
@@ -85,14 +85,13 @@ class Dashboard extends StatelessWidget {
 
   Widget balanceCard() {
     return Container(
-
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: AlignmentGeometry.centerLeft,
           end: AlignmentGeometry.centerRight,
           colors: [Color(0xFF9333ea), Color(0xFF3b82f6)],
         ),
-        borderRadius: BorderRadius.circular(15)
+        borderRadius: BorderRadius.circular(15),
       ),
       padding: EdgeInsets.all(20),
       child: Column(
@@ -103,6 +102,86 @@ class Dashboard extends StatelessWidget {
             children: [
               Text("Total Balance", style: TextStyle(color: Colors.white)),
               Container(child: Icon(Icons.credit_card, color: Colors.white)),
+            ],
+          ),
+          Text(
+            "\$12,456.50",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.u_turn_left,
+                            color: Colors.green,
+                            size: 20,
+                          ),
+                          SizedBox(width: 5),
+                          Text("Income", style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                      Text(
+                        "\$12,456.50",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.u_turn_right_rounded,
+                            color: Colors.red,
+                            size: 20,
+                          ),
+                          SizedBox(width: 5),
+                          Text("Expenses", style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                      Text(
+                        "\$12,456.50",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ],
